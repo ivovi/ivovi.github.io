3f5eafd7a0c17293d7bbf9fa1a2c45d1b7bd8015
ivovi <ivo.murdoch@uq.net.au>
2022-05-17 16:32:07 +1000

    Removed code about fullscreen button to prevent errors.

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
 var loadingBar = document.querySelector("#unity-loading-bar");
 var progressBarFull = document.querySelector("#unity-progress-bar-full");
-var fullscreenButton = document.querySelector("#unity-fullscreen-button");
 var warningBanner = document.querySelector("#unity-warning");
 
 // Shows a temporary message banner/ribbon for a few seconds, or
@@ -99,9 +98,6 @@ script.onload = () => {
 		progressBarFull.style.width = 100 * progress + "%";
 	}).then((unityInstance) => {
 		loadingBar.style.display = "none";
-		fullscreenButton.onclick = () => {
-			unityInstance.SetFullscreen(1);
-		};
 	}).catch((message) => {
 		alert(message);
 	});
